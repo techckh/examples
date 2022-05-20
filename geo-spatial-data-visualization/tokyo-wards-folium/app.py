e60af15ef6acf3faabe07897fcba3d24e5837ecf
Scott <you@example.com>
2022-05-20 18:28:09 +0800

    add geo visualization code

--- geo-spatial-data-visualization/tokyo-wards-folium/app.py
+++ geo-spatial-data-visualization/tokyo-wards-folium/app.py
@@ -32,6 +32,35 @@ def get_tokyo_wards_map(df):
     return map_tokyo
 
 
+def get_tokyo_yamanote_line_map(df):
+    map_tokyo = folium.Map(location=[35.6828387, 139.7594549], zoom_start=11)
+    for lat, lng, label in zip(df['lat'], df['lon'], df['station']):
+        label = folium.Popup(label, parse_html=True)
+        """
+        folium.CircleMarker(
+            [lat, lng],
+            radius=5,
+            popup=label,
+            color='blue',
+            fill=True,
+            fill_color='#3186cc',
+            fill_opacity=0.2,
+            parse_html=False).add_to(map_tokyo)
+        """
+
+        folium.Circle(
+            [lat, lng],
+            radius=700,
+            popup=label,
+            color='blue',
+            fill=True,
+            fill_color='#3186cc',
+            fill_opacity=0.2,
+            parse_html=False).add_to(map_tokyo)
+
+    return map_tokyo
+
+
 def get_tokyo_wards_map_cartodb(df):
     #geojson_path = '../data/tokyo13.json'
     geojson_path = '../data/tokyo23.json'
@@ -46,8 +75,27 @@ if __name__ == '__main__':
 
     #download_process_tokyo_wards_data()
 
+    """
     df = pd.read_pickle('../data/tokyo_wards.pkl')
     map = get_tokyo_wards_map(df)
     #map = get_tokyo_wards_map_cartodb(df)
     map.save("map.html")
     webbrowser.open("map.html")
+    """
+
+    df = pd.read_pickle('../data/tokyo_yamanote_line.pkl')
+    # corrections
+    # TODO: tokyo akihabara
+    df.loc[df.station == 'Shinagawa', 'lat'] = 35.628611
+    df.loc[df.station == 'Shinagawa', 'lon'] = 139.739167
+    df.loc[df.station == 'Ebisu', 'lat'] = 35.646643
+    df.loc[df.station == 'Ebisu', 'lon'] = 139.710045
+    df.loc[df.station == 'Shibuya', 'lat'] = 35.658514
+    df.loc[df.station == 'Shibuya', 'lon'] = 139.70133
+    df.loc[df.station == 'Gotanda', 'lat'] = 35.62645
+    df.loc[df.station == 'Gotanda', 'lon'] = 139.7234
+    df.loc[df.station == 'Meguro', 'lat'] = 35.633983
+    df.loc[df.station == 'Meguro', 'lon'] = 139.716
+    map = get_tokyo_yamanote_line_map(df)
+    map.save("map_yamanote.html")
+    webbrowser.open("map_yamanote.html")
